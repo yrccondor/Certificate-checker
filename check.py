552bf86743d4ab2dcf0627d48c98bf15bd61b7db
unknown <yrc371106@hotmail.com>
2018-01-16 13:30:18 +0800

    Add print types

--- check.py
+++ check.py
@@ -6,15 +6,27 @@ import time
  
 with open('config.json', 'r', encoding='UTF-8') as fr:
     config = json.load(fr)
+output_type = config.get('output', "print")
+if output_type == "print_details":
+    print("["+time.strftime("%H:%M:%S", time.localtime())+"] Read config.json successfully.")
 websitelist = config['website_list']
-print('------------')
+if output_type == "print_details":
+    print("["+time.strftime("%H:%M:%S", time.localtime())+"] Read website_list successfully. "+str(len(websitelist))+" web site(s) will be scaned.")
 time_out = str(config.get('timeout', 10))
-sleep_time = str(config.get('sleep', 2))
+if output_type == "print_details":
+    print("["+time.strftime("%H:%M:%S", time.localtime())+"] Read timeout successfully. Value: "+str(time_out)+"s")
+sleep_time = config.get('sleep', 2)
+if output_type == "print_details":
+    print("["+time.strftime("%H:%M:%S", time.localtime())+"] Read output successfully. Value: "+str(sleep_time)+"s")
 for i in range(len(websitelist)):
+    if output_type == "print_details":
+        print("["+time.strftime("%H:%M:%S", time.localtime())+"] Scanning "+websitelist[i]+"("+str(i+1)+" in "+str(len(websitelist))+").")
     comm0 = "curl https://"+websitelist[i]+" --connect-timeout "+time_out+" -v -s -o /dev/null 2>/tmp/ca.info ; cat /tmp/ca.info"
     out_bytes0 = subprocess.check_output(comm0, shell=True)
     out_text0 = out_bytes0.decode('utf-8')
     if len(out_text0)>370:
+        if output_type == "print_details":
+            print("["+time.strftime("%H:%M:%S", time.localtime())+"] Read reponse successfully.")
         comm1 = "cat /tmp/ca.info | grep 'start date: '" #利用curl检查证书开始时间
         out_bytes1 = subprocess.check_output(comm1, shell=True)
         out_text1 = out_bytes1.decode('utf-8')
@@ -27,7 +39,15 @@ for i in range(len(websitelist)):
         comm4 = "cat /tmp/ca.info | grep 'common name: '"
         out_bytes4 = subprocess.check_output(comm4, shell=True)
         out_text4 = out_bytes4.decode('utf-8')
+        if output_type == "print_details":
+            print("["+time.strftime("%H:%M:%S", time.localtime())+"] Read certificate infomation successfully.")
         os.system('rm -f /tmp/ca.info')
+        if output_type == "print_details":
+            print("["+time.strftime("%H:%M:%S", time.localtime())+"] Environment cleaned.")
+        if i == 0:
+            print('-----------------------------')
+        elif output_type == "print_details":
+            print('-----------------------------')
         print('Certificate infomation about '+websitelist[i]+':')
         print('')
         print('Check time: '+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
@@ -35,8 +55,15 @@ for i in range(len(websitelist)):
         print('End: '+time.strftime("%Y-%m-%d %H:%M:%S",time.strptime(out_text2[-25:-5],"%b %d %H:%M:%S %Y")))
         print('Common Name: '+out_text4[16:-1])
         print('Issuer Info: '+out_text3[11:-1])
-        print('------------')
+        print('-----------------------------')
     else:
+        if output_type == "print_details":
+            print("["+time.strftime("%H:%M:%S", time.localtime())+"] No reponse(Time out).")
+        if i == 0:
+            print('-----------------------------')
+        elif output_type == "print_details":
+            print('-----------------------------')
         print('Error: Connect Failed.')
-        print('------------')
+        print('-----------------------------')
+        os.system('rm -f /tmp/ca.info')
     time.sleep(sleep_time)
